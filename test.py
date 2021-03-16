fb67e40fb79d14738960126c0c8e942717abff87
Sulaiman3352 <32817650+Sulaiman3352@users.noreply.github.com>
2021-03-16 23:08:28 +0300

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-
-a = "https://dc716.4shared.com/img/IzK6X42Miq/d7f69b6e/dlink__2Fdownload_2FIzK6X42Miq_3Fsbsr_3D6fd89756c3802ca8e3c3db1df0138eeaa6d_26bip_3DMTk2LjE5MC44OC4yMDg_26lgfp_3D66_26bip_3DMTk2LjE5MC44OC4yMDg_26bip_3DMTk2LjE5MC44OC4yMDg/preview.mp4"
-
-b = "انمي Boruto: Naruto Next Generations الحلقة 1 مترجمة اون لاين - انمي فور اب"
-
-urllib.request.urlretrieve(a, b) 
